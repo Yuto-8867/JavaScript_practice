9e4d2e07e67651b0384f5ffdaf7590d6a2926843
YutoInoue <baseball.taki.yi.1994@gmail.com>
2021-07-16 01:17:41 +0900

    avg max

--- Practice.js
+++ Practice.js
@@ -1,14 +1,14 @@
-var number = 1;
+var scores = [88, 62, 65, 21, 47, 92, 57, 89, 79, 89, 54, 82, 69, 72, 74, 54, 66, 92, 64, 96, 47, 89, 95, 93, 70, 30, 84, 93, 81, 98, 78, 96, 32, 56, 64, 42, 67];
 
-while(number <= 100) {
-  if(number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz");
-  }else if(number % 5 === 0) {
-    console.log("Buzz");
-  }else if(number % 3 === 0) {
-    console.log("Fizz")
-  }else {
-    console.log(number);
+var sum = 0;
+var max = 0;
+for(var i = 0; i < scores.length; i ++) {
+  sum += scores[i];
+  if(max < scores[i]) {
+    max = scores[i];
   }
-  number ++;
 }
+var avg = sum / scores.length;
+console.log("平均点は" + avg + "です");
+console.log("最高点は" + max + "です");
+
