689c6289e9050bb2b0c6fc6f522abb49f3be1c56
YutoInoue <baseball.taki.yi.1994@gmail.com>
2021-07-16 13:42:31 +0900

    method class

--- Practice.js
+++ Practice.js
@@ -1,14 +1,40 @@
-var scores = [88, 62, 65, 21, 47, 92, 57, 89, 79, 89, 54, 82, 69, 72, 74, 54, 66, 92, 64, 96, 47, 89, 95, 93, 70, 30, 84, 93, 81, 98, 78, 96, 32, 56, 64, 42, 67];
+class Animal {
+  constructor(name,age) {
+    this.name = name;
+    this.age = age;
+  }
 
-var sum = 0;
-var max = 0;
-for(var i = 0; i < scores.length; i ++) {
-  sum += scores[i];
-  if(max < scores[i]) {
-    max = scores[i];
+  greet() {
+    console.log("こんにちは");
+  }
+
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
   }
 }
-var avg = sum / scores.length;
-console.log("平均点は" + avg + "です");
-console.log("最高点は" + max + "です");
 
+class Dog extends Animal {
+  constructor(name,age,breed) {
+    super(name,age);
+    this.breed = breed;
+  }
+
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`犬種は${this.breed}です`);
+    console.log(`${this.age}歳です`);
+
+    const humanAge = dog.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
+  }
+
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+const dog = new Dog("ジョン",7,"チワワ");
+dog.info();
